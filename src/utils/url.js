6786d554e32acc859c8f126282d31c27b2172092
Jaivik Tailor <jaivik.tailor@dal.ca>
2023-04-07 15:31:54 -0300

    URL Added

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,3 @@
-const BASE_URL = "http://localhost:5000/";//http://todolist-env-1.eba-uyyguzmn.us-east-1.elasticbeanstalk.com/
+const BASE_URL = "http://jaiviknotogo-env.eba-7mmixuaw.us-east-1.elasticbeanstalk.com/";
+//const BASE_URL = "http://localhost:5000/"
 export default BASE_URL;
